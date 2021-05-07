1b7992f8f6939d386f4d8dc071770ab095c7bafe
Diego Cotelo <diego.cotelo@caylent.com>
2021-05-07 17:16:54 -0300

    Example

--- test/caylent-ecs.test.ts
+++ test/caylent-ecs.test.ts
@@ -1,13 +1,44 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as CaylentEcs from '../lib/caylent-ecs-stack';
+import '@aws-cdk/assert/jest';
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new CaylentEcs.CaylentEcsStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+const app = new cdk.App();
+// WHEN
+const stack = new CaylentEcs.CaylentEcsStack(app, 'MyTestStack');
+
+/* test('Empty Stack', () => {
+
+  // THEN
+  expectCDK(stack).to(matchTemplate({
+    "Resources": {}
+  }, MatchStyle.EXACT))
+}); */
+
+test('VPC', () => {
+  expect(stack).toHaveResource('AWS::EC2::VPC', {
+    EnableDnsHostnames: true,
+    EnableDnsSupport: true,
+  });
+});
+
+test('Internet Access', () => {
+  expect(stack).toHaveResource('AWS::EC2::Route', {
+    DestinationCidrBlock: "0.0.0.0/0"
+  });
+});
+
+test('ECS-task definition', () => {
+  expect(stack).toHaveResource('AWS::ECS::TaskDefinition', {
+    Cpu: "512",
+    Memory: "2048",
+  });
+});
+
+test('ECS-Service', () => {
+  expect(stack).toHaveResource("AWS::ECS::Service", {
+    LaunchType: 'FARGATE',
+    DesiredCount: 6,
+    HealthCheckGracePeriodSeconds: 60
+  });
 });
