de8fcc6c713e39b1b387c26e23c4fe741dfe514d
Diego Cotelo <diego.cotelo13@gmail.com>
2022-05-21 17:29:27 -0300

    refactor names

--- test/dynamodb.test.ts
+++ test/dynamodb.test.ts
@@ -1,13 +1,13 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as CaylentEcs from '../lib/caylent-ecs-stack';
+import * as CaylentEcs from '../lib/ecs-stack';
 import '@aws-cdk/assert/jest';
 import { SynthUtils } from '@aws-cdk/assert';
 
 const app = new cdk.App();
 // WHEN
 
-const stack = new CaylentEcs.CaylentEcsStack(app, 'MyDynamoTestStack');
+const stack = new CaylentEcs.EcsStack(app, 'MyDynamoTestStack');
 
 test('DynamoDB - Snapshot', () => {
     expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
